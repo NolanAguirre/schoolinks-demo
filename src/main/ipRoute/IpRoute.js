29e56dde3217235eaf692bcf30adf0574f49a53c
Nolan Aguirre <NolanAguirre08@gmail.com>
2019-04-09 14:08:00 -0500

    change componentWillMount to componentDidMount

--- src/main/ipRoute/IpRoute.js
+++ src/main/ipRoute/IpRoute.js
@@ -10,7 +10,7 @@ class IpRoute extends Component{
             loading:false
         }
     }
-    componentWillMount = () => {
+    componentDidMount = () => {
         this.handleSubmit()
     }
 
